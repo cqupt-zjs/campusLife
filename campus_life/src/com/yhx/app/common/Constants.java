bb2f06d315ad205f50e1c578eac48719cf579771
cqupt-zjs <1250656856@qq.com>
2015-10-19 08:15:10 +0800

    第一次提交
    校园二手交易app源码

---
+++ campus_life/src/com/yhx/app/common/Constants.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.yhx.app.common;
+
+/**
+ * @author zjs
+ *
+ */
+public class Constants {
+	/** 与服务器端连接的协议名 */
+	public static final String PROTOCOL = "http://";
+	/** 服务器IP */
+	public static final String HOST = "202.202.43.87";//AVD test
+	/** 服务器端口号 */
+	public static final String PORT = "80";
+	public static final String URL = PROTOCOL+HOST+":"+PORT; 
+}
